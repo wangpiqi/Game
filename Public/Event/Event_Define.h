87380fd9ee0bdd542a55437721644d0f306f9ea3
wangpiqi <737711464@qq.com>
2017-08-09 21:31:18 +0800

    1

--- Public/Event/Event_Define.h
+++ Public/Event/Event_Define.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "FastDelegate.h"
-#include "Variant\VarList.h"
+#include "..\Variant\VarList.h"
 
 typedef fastdelegate::FastDelegate2<const char*, VarList&, void> EventDelegate;
 typedef std::map<std::string, EventDelegate> EventDelegateMap;
